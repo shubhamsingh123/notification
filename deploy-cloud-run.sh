182c9114e80a0b136e63fba4f360062a204e3739
mithun <mithunwalawalkar100@gmail.com>
2025-07-14 12:28:43 +0530

    added docker and deployed to cloud run

---
+++ deploy-cloud-run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set variables
+PROJECT_ID="telusrecruitai"
+REGION="us-central1"  # Default region, can be changed
+IMAGE_NAME="notification-service"
+SERVICE_NAME="notification-service"
+
+# Build the Docker image
+docker build -t $IMAGE_NAME .
+
+# Tag the image for Google Container Registry
+docker tag $IMAGE_NAME gcr.io/$PROJECT_ID/$IMAGE_NAME
+
+# Push the image to Google Container Registry
+docker push gcr.io/$PROJECT_ID/$IMAGE_NAME
+
+# Deploy to Cloud Run
+gcloud run deploy $SERVICE_NAME \
+  --image gcr.io/$PROJECT_ID/$IMAGE_NAME \
+  --platform managed \
+  --region $REGION \
+  --project $PROJECT_ID \
+  --allow-unauthenticated
