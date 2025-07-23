e7dae022697b88e833954436434d2b38550edebb
shubhamsingh123 <shubham16cse06@gmail.com>
2025-07-23 12:34:02 +0530

    pdf styling changes done

--- src/main/java/com/telus/notification/util/PdfGenerator.java
+++ src/main/java/com/telus/notification/util/PdfGenerator.java
@@ -2,42 +2,79 @@ package com.telus.notification.util;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Image;
 import com.telus.notification.model.InterviewQuestionsEmailModel;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
+import java.net.URL;
 import java.time.format.DateTimeFormatter;
 
 @Component
 public class PdfGenerator {
 
+    private static final String LOGO_URL = "https://storage.googleapis.com/telus_recruit_ai/Telus_logo_logotype.png";
+    private static final BaseColor PRIMARY_COLOR = new BaseColor(75, 40, 109); // Dark Purple
+    private static final BaseColor SECONDARY_COLOR = new BaseColor(51, 51, 51); // Dark Gray
+
     public byte[] generateInterviewQuestionsPdf(InterviewQuestionsEmailModel model) throws DocumentException {
-        Document document = new Document();
+        Document document = new Document(PageSize.A4, 50, 50, 50, 50); // Margins: 50pt on all sides
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        PdfWriter.getInstance(document, out);
-        document.open();
+        try {
+            PdfWriter.getInstance(document, out);
+            document.open();
 
-        Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18, BaseColor.BLACK);
-        Font normalFont = FontFactory.getFont(FontFactory.HELVETICA, 12, BaseColor.BLACK);
+            // Add logo
+            Image logo = Image.getInstance(new URL(LOGO_URL));
+            logo.scaleToFit(200, 100); // Scale logo to reasonable size
+            logo.setAlignment(Element.ALIGN_CENTER);
+            document.add(logo);
+            document.add(Chunk.NEWLINE);
 
-        Paragraph title = new Paragraph("Interview Questions", titleFont);
-        title.setAlignment(Element.ALIGN_CENTER);
-        document.add(title);
-        document.add(Chunk.NEWLINE);
+            // Fonts
+            Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 24, PRIMARY_COLOR);
+            Font subtitleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 14, SECONDARY_COLOR);
+            Font normalFont = FontFactory.getFont(FontFactory.HELVETICA, 12, SECONDARY_COLOR);
+            Font questionFont = FontFactory.getFont(FontFactory.HELVETICA, 12, PRIMARY_COLOR);
 
-        document.add(new Paragraph("Candidate: " + model.getCandidateName(), normalFont));
-        document.add(new Paragraph("Position: " + model.getPosition(), normalFont));
-        document.add(new Paragraph("Interview Date: " + 
-            model.getInterviewDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")), normalFont));
-        document.add(Chunk.NEWLINE);
+            // Title
+            Paragraph title = new Paragraph("Interview Questions", titleFont);
+            title.setAlignment(Element.ALIGN_CENTER);
+            title.setSpacingAfter(30);
+            document.add(title);
 
-        document.add(new Paragraph("Questions:", normalFont));
-        for (int i = 0; i < model.getQuestions().size(); i++) {
-            document.add(new Paragraph((i + 1) + ". " + model.getQuestions().get(i), normalFont));
+            // Candidate Info Section
+            Paragraph candidateInfo = new Paragraph();
+            candidateInfo.add(new Chunk("Candidate: ", subtitleFont));
+            candidateInfo.add(new Chunk(model.getCandidateName() + "\n", normalFont));
+            candidateInfo.add(new Chunk("Position: ", subtitleFont));
+            candidateInfo.add(new Chunk(model.getPosition() + "\n", normalFont));
+            candidateInfo.add(new Chunk("Interview Date: ", subtitleFont));
+            candidateInfo.add(new Chunk(
+                model.getInterviewDate().format(DateTimeFormatter.ofPattern("MMMM dd, yyyy HH:mm")), 
+                normalFont
+            ));
+            candidateInfo.setSpacingAfter(30);
+            document.add(candidateInfo);
+
+            // Questions Section
+            Paragraph questionsTitle = new Paragraph("Interview Questions:", subtitleFont);
+            questionsTitle.setSpacingAfter(20);
+            document.add(questionsTitle);
+
+            for (int i = 0; i < model.getQuestions().size(); i++) {
+                Paragraph question = new Paragraph();
+                question.add(new Chunk((i + 1) + ". ", questionFont));
+                question.add(new Chunk(model.getQuestions().get(i), normalFont));
+                question.setSpacingAfter(15);
+                document.add(question);
+            }
+
+            document.close();
+            return out.toByteArray();
+        } catch (Exception e) {
+            throw new DocumentException("Failed to generate PDF: " + e.getMessage());
         }
-
-        document.close();
-        return out.toByteArray();
     }
 }
